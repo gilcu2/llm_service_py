72dff578cd415a681e2765a06c6a1883cf701e4e
Reynaldo Gil <gilcu2mail.com>
2025-02-26 11:16:24 +0100

    Tag 0.1.1

---
+++ scripts/git_tag.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+BUMP_RULE=${1:-patch}
+
+cd $PROJECT_DIR
+
+poetry version $BUMP_RULE
+NEW_VERSION=$(poetry version --short)
+
+kacl-cli release "$NEW_VERSION" --modify --auto-link
+TAG_BODY=$(kacl-cli get "$NEW_VERSION")
+
+git add CHANGELOG.md pyproject.toml
+git commit -m "Tag $NEW_VERSION"
+
+git tag -a "$NEW_VERSION" -m "$TAG_BODY"
